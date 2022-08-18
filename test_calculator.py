39fb140220ddff0fcb2df6720b06a8d01f12eef1
Alexander Kobilinsky <sarulon@gmail.com>
2022-08-18 18:02:41 +0300

    add test form multiplication

--- test_calculator.py
+++ test_calculator.py
@@ -8,3 +8,7 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 2 == calculator.substruct(4,2)
+
+
+    def test_multiplication(self):
+        assert calculator.multiply(10,10)
